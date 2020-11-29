fc2456c536fdf7effddd597acfa33b4b31ae65df
Vlad Duma <vlad.duma@yahoo.co.uk>
2020-11-29 16:50:57 +0200

    Fixed the bug where multiple actions are done when pressing one key

--- SMC_Project_Tetris++/SMC_Project_Tetris++/Main.cpp
+++ SMC_Project_Tetris++/SMC_Project_Tetris++/Main.cpp
@@ -8,46 +8,45 @@ int main()
 	sf::Texture texture;
 	if (!texture.loadFromFile("Blocks.png"))
 		std::cout << "Can't load the texture from the file !" << std::endl;
-	
-	TetrisShape shape(texture, 5);
+
+	TetrisShape shape(texture, 0);
 
 	while (window.isOpen())
 	{
 		sf::Event event;
+		window.clear();
+		window.draw(shape);
 		while (window.pollEvent(event))
+		{
 			if (event.type == sf::Event::Closed)
 				window.close();
-	
-		window.clear();
-		window.draw(shape); 
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-		{
-			shape.Rotate(); 
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-		{
-			shape.Move(Direction::Left);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-		{
-			shape.Move(Direction::Right);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-		{
-			shape.Move(Direction::Down);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-		{
-			shape.ScaleUp();
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
-		{
-			shape.ScaleDown();
+			if (event.type == sf::Event::KeyPressed)
+			{
+				switch (event.key.code)
+				{
+				case(sf::Keyboard::Escape):
+					window.close();
+					break;
+				case(sf::Keyboard::Up):
+					shape.Rotate();
+					break;
+				case(sf::Keyboard::Down):
+					shape.Move(Direction::Down);
+					break;
+				case(sf::Keyboard::Left):
+					shape.Move(Direction::Left);
+					break;
+				case(sf::Keyboard::Right):
+					shape.Move(Direction::Right);
+					break;
+				default:
+					break;
+				}
+			}
 		}
 
 		window.display();
 	}
-	
+
 	return 0;
-}        
+}
