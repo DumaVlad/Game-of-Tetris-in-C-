d06c40d0a443b31afe5053ab97e894787717fb4a
VladimirC <cojocaruvladimir@yahoo.com>
2020-12-05 11:40:46 +0200

    Made the ScaleUP method properly

--- SMC_Project_Tetris++/SMC_Project_Tetris++/TetrisShape.cpp
+++ SMC_Project_Tetris++/SMC_Project_Tetris++/TetrisShape.cpp
@@ -3,41 +3,45 @@
 #include <iostream>
 
 /*BlockArray5x5 // The 5x5 array used to store a Shape
-[0, 1, 2, 3, 4]
-[5, 6, 7, 8, 9]
-[10, 11, 12, 13, 14]
-[15, 16, 17, 18, 19]
-[20, 21, 22, 23, 24]
+[ 0,  1,  2,  3,  4,  5,  6,  7]
+[ 8,  9, 10, 11, 12, 13, 14, 15]
+[16, 17, 18, 19, 20, 21, 22, 23]
+[24, 25, 26, 27, 28, 29, 30, 31]
+[32, 33, 34, 35, 36, 37, 38, 39]
+[40, 41, 42, 43, 44, 45, 46, 47]
+[48, 49, 50, 51, 52, 53, 54, 55]
+[56, 57, 58, 59, 60, 61, 62, 63]
+
 */
 
-unsigned int BlockArray5x5[7][9] = {
-	{6,7,11,12,6,6,6,6,6}, //SquareShape
-	{7,12,17,18,7,7,7,7,7}, // LShape
-	{7,12,17,16,7,7,7,7,7}, // JShape
-	{6,11,12,17,6,6,6,6,6}, // SShape
-	{7,12,11,16,7,7,7,7,7}, // ZShape
-	{7,11,12,13,7,7,7,7,7}, // TShape
-	{7,12,17,22,7,7,7,7,7} // IShape
+unsigned int BlockArray5x5[7][16] = {
+	{27,28,35,36,27,27,27,27,27,27,27,27,27,27,27,27}, //SquareShape
+	{27,35,43,44,27,27,27,27,27,27,27,27,27,27,27,27}, // LShape
+	{27,35,43,42,27,27,27,27,27,27,27,27,27,27,27,27}, // JShape
+	{27,28,35,34,27,27,27,27,27,27,27,27,27,27,27,27}, // SShape
+	{27,28,36,37,27,27,27,27,27,27,27,27,27,27,27,27}, // ZShape
+	{27,35,34,36,27,27,27,27,27,27,27,27,27,27,27,27}, // TShape
+	{19,27,35,43,27,27,27,27,27,27,27,27,27,27,27,27} // IShape
 };
 
-unsigned int ScaledBlockArray5x5[7][9] = {
-	{6,7,8,11,12,13,16,17,18}, //SquareShape
-	{2,7,12,17,18,19,2,2,2}, // LShape
-	{2,7,12,17,16,15,2,2,2}, // JShape
-	{1,6,11,12,13,18,23,1,1}, // SShape
-	{2,7,12,11,10,15,20,2,2}, // ZShape
-	{2,7,10,11,12,13,14,7,7}, // TShape
-	{2,7,12,17,22,2,2,2,2} // IShape
+unsigned int ScaledBlockArray5x5[7][16] = {
+	{18,19,20,21,26,27,28,29,34,35,36,37,42,43,44,45}, //SquareShape
+	{11,12,19,20,27,28,35,36,43,44,51,52,45,46,53,54}, // LShape
+	{11,12,19,20,27,28,35,36,43,44,51,52,41,42,49,50}, // JShape
+	{19,20,21,22,27,28,29,30,33,34,35,36,41,42,43,44}, // SShape
+	{17,18,19,20,25,26,27,28,35,36,37,38,43,44,45,46}, // ZShape
+	{19,20,27,28,35,36,43,44,33,34,41,42,37,38,45,46}, // TShape
+	{ 3, 4,11,12,19,20,27,28,35,36,43,44,51,52,59,60} // IShape
 };
 
 TetrisShape::TetrisShape(sf::Texture& texture, uint16_t id)
 	: m_Position{ Position{5,5} }, m_CurrentRotation{ 0 }, m_ID{ id }, m_Block{}, m_Sprite{ texture, sf::IntRect{(id % 7) * 18, 0, 18, 18} }
 {
 	m_ID = m_ID % 7; // In case of an id > 6, we make sure that is not out of bounds
-	for (int index = 0; index < 9; index++)
+	for (int index = 0; index < 16; index++)
 	{
-		m_Block[index].x = BlockArray5x5[m_ID][index] % 5;
-		m_Block[index].y = BlockArray5x5[m_ID][index] / 5;
+		m_Block[index].x = BlockArray5x5[m_ID][index] % 8;
+		m_Block[index].y = BlockArray5x5[m_ID][index] / 8;
 	}
 }
 
@@ -46,19 +50,19 @@ uint16_t TetrisShape::GetID() const
 	return m_ID;
 }
 
-std::array<TetrisShape::Position, 9> TetrisShape::GetBlockPosition() const
+std::array<TetrisShape::Position, 16> TetrisShape::GetBlockPosition() const
 {
-	std::array<Position, 9> blockPosition;
-	for (int i = 0; i < 4; i++)
+	std::array<Position, 16> blockPosition;
+	for (int i = 0; i < 16; i++)
 	{
 		blockPosition[i] = Position{ m_Block[i].x + m_Position.x, m_Block[i].y + m_Position.y };
 	}
 	return blockPosition;
 }
 
-std::array<TetrisShape::Position, 9> TetrisShape::GetFutureBlockPosition(Direction direction) const
+std::array<TetrisShape::Position, 16> TetrisShape::GetFutureBlockPosition(Direction direction) const
 {
-	std::array<Position, 9> blockPositions;
+	std::array<Position, 16> blockPositions;
 	Position tempPosition{ m_Position };
 
 	if (direction == Direction::Left)
@@ -68,7 +72,7 @@ std::array<TetrisShape::Position, 9> TetrisShape::GetFutureBlockPosition(Directi
 	else
 		tempPosition.y++;
 
-	for (auto i = 0; i < 9; ++i) {
+	for (auto i = 0; i < 16; ++i) {
 		blockPositions[i] = Position{ m_Block[i].x + tempPosition.x, m_Block[i].y + tempPosition.y };
 	}
 	return blockPositions;
@@ -93,7 +97,7 @@ void TetrisShape::Rotate()
 	}
 	if (m_ID == 6) { // IShape: restrict "rotation" to two states (horizontal/vertical)
 		m_CurrentRotation++;
-		for (auto i = 0; i < 9; ++i) {
+		for (auto i = 0; i < 16; ++i) {
 			Position oldPoint = m_Block[i];    //pivot
 			Position localVector = oldPoint - Position{ 1, 2 };
 			Position nextPoint{};
@@ -116,7 +120,7 @@ void TetrisShape::Rotate()
 		}
 		return;
 	}
-	for (auto i = 0; i < 9; ++i) {
+	for (auto i = 0; i < 16; ++i) {
 		Position oldPoint = m_Block[i];    //pivot
 		Position localVector = oldPoint - Position{ 1,2 };
 
@@ -147,25 +151,25 @@ void TetrisShape::Move(Direction direction)
 
 void TetrisShape::ScaleUp()
 {
-	for (int i = 0; i < 9; i++)
+	for (int i = 0; i < 16; i++)
 	{
-		m_Block[i].x = ScaledBlockArray5x5[m_ID][i] % 5;
-		m_Block[i].y = ScaledBlockArray5x5[m_ID][i] / 5;
+		m_Block[i].x = ScaledBlockArray5x5[m_ID][i] % 8;
+		m_Block[i].y = ScaledBlockArray5x5[m_ID][i] / 8;
 	}
 }
 
 void TetrisShape::ScaleDown()
 {
-	for (int i = 0; i < 9; i++)
+	for (int i = 0; i < 16; i++)
 	{
-		m_Block[i].x = BlockArray5x5[m_ID][i] % 5;
-		m_Block[i].y = BlockArray5x5[m_ID][i] / 5;
+		m_Block[i].x = BlockArray5x5[m_ID][i] % 8;
+		m_Block[i].y = BlockArray5x5[m_ID][i] / 8;
 	}
 }
 
 void TetrisShape::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-	for (int i = 0; i < 9; i++)
+	for (int i = 0; i < 16; i++)
 	{
 		m_Sprite.setPosition((m_Block[i].x * 18) + (m_Position.x * 18), (m_Block[i].y * 18) + (m_Position.y * 18));
 		target.draw(m_Sprite);
