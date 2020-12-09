3ead0da4fc3e9fd7abde771fd9ca03d2d5603cfd
LaurentiuAndrei <laurandrei994@gmail.com>
2020-12-09 16:30:46 +0200

    Implementing the Score Class to help us manage the score of the player

---
+++ SMC_Project_Tetris++/SMC_Project_Tetris++/Score.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Text.hpp>
+
+class Score
+{
+public:
+	Score();
+	Score(const Score& score) = delete;
+	Score& operator=(const Score& score) = delete;
+
+	void Draw(sf::RenderWindow& window);
+	void Reset();
+	void AddPressedScore(int score);
+	void SumPressedScore();
+	void AddClearedLines(int number);
+	void Update(const sf::Time& dt);
+	int GetLevel() const;
+
+private:
+	sf::Font m_Font;
+	sf::Text m_LevelText;
+	sf::Text m_ScoreText;
+	sf::Text m_ClearedLinesText;
+	int m_Score;
+	int m_LoadPressedScore;
+	int m_LinesCleared;
+};
+
