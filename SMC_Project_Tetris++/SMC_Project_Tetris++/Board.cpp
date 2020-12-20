73f42f8336866ce2021e4533710b661d910bf56c
Duma Vlad <vlad.duma@yahoo.co.uk>
2020-12-20 13:12:06 +0200

    Rewrote the Board class for new implementation

--- SMC_Project_Tetris++/SMC_Project_Tetris++/Board.cpp
+++ SMC_Project_Tetris++/SMC_Project_Tetris++/Board.cpp
@@ -2,141 +2,12 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <SFML/Audio/Music.hpp>
+#include "Utils.h"
 
-FieldInfo::FieldInfo(sf::Texture& texture, uint16_t id)
+Board::Board(Position size, IGame& game)
+	: IBoard{ size, game }
 {
-	sf::IntRect rectangle{ (id % 7) * 18, 0, 18, 18 };
-	m_Sprite.setTexture(texture);
-	m_Sprite.setTextureRect(rectangle);
-}
-
-Field& Field::operator=(const Field& field)
-{
-	this->m_Occupied = field.m_Occupied;
-	this->m_Visible = field.m_Visible;
-	this->m_Info = field.m_Info;
-
-	return *this;
-}
-
-Board::Board(Position size, Game& game)
-	: m_Game{ game }, m_Fields{}, m_FieldInfos{}, m_Size{ size }, m_ToBeCleaned{}, m_ElapsedTime{ 0.f }, m_ToRemoveBlocks{ false }
-{
-	for (int x = 0; x < size.x; x++)
-		for (int y = 0; y < size.y; y++)
-			m_Fields[Convert2DTo1D(x, y)] = std::make_unique<Field>();
-
-	for (int id = 0; id < 7; id++)
-		m_FieldInfos[id] = std::make_unique<FieldInfo>(m_Game.m_Texture, id);
-}
-
-void Board::Update(const sf::Time& dt)
-{
-	MarkLinesForRemoval();
-	if (m_ToRemoveBlocks)
-	{
-		m_ElapsedTime += dt.asSeconds();
-		Blink();
-		if (m_ElapsedTime > 0.6f)
-		{
-			m_ElapsedTime = 0.f;
-			CleanLines();
-		}
-	}
-}
-
-void Board::Clean()
-{
-	for (uint16_t x = 0; x < m_Size.x; x++)
-	{
-		for (uint16_t y = 0; y < m_Size.y; y++)
-		{
-			auto field = GetField(x, y);
-			field->m_Occupied = false;
-			field->m_Visible = true;
-			field->m_Info = nullptr;
-		}
-	}
-}
-
-void Board::AddBlock(uint16_t id, std::array<Position, 16> block)
-{
-	for (int i = 0; i < 16; i++)
-	{
-		auto field = GetField(block[i].x, block[i].y);
-		field->m_Occupied = true;
-		field->m_Info = m_FieldInfos[id].get();
-	}
-}
-
-bool Board::IsOccupied(std::array<Position, 16> block)
-{
-	for (int i = 0; i < 16; i++)
-	{
-		auto field = GetField(block[i].x, block[i].y);
-		if (field->m_Occupied)
-			return true;
-	}
-	return false;
-}
-
-Field* Board::GetField(uint16_t x, uint16_t y)
-{
-	return m_Fields[Convert2DTo1D(x, y)].get();
-}
-
-void Board::Draw(sf::RenderWindow& window)
-{
-	for (int x = 0; x < m_Size.x; x++)
-		for (int y = 0; y < m_Size.y; y++)
-		{
-			auto field = GetField(x, y);
-
-			//if field isn't occupied yet, m_Info is set to nullptr
-			if (field->m_Occupied && field->m_Visible)
-			{
-				field->m_Info->m_Sprite.setPosition(x * 18.f, y * 18.f);
-				window.draw(field->m_Info->m_Sprite);
-			}
-		}
-}
-
-
-int Board::Convert2DTo1D(uint16_t x, uint16_t y)
-{
-	return (y * m_Size.x + x);
-}
-
-void Board::CleanLines()
-{
-	if (m_ToBeCleaned.empty())
-		return;
-
-	sf::Music clearLinesMusic;
-	if (!clearLinesMusic.openFromFile("clear.wav"))
-		std::cout << "Could not load ~clear.wav~ from file !! \n";
-	m_Game.m_GameplayMusic.stop();
-	clearLinesMusic.play();
-	clearLinesMusic.setLoop(true);
-	
-	for (auto i : m_ToBeCleaned)
-	{
-		for (auto y = i; y >= 0; y--)
-		{
-			for (auto x = 0; x < m_Size.x; x++)
-			{
-				int up = y - 1;
-				if (up < 0)
-					continue;
-				*GetField(x, y) = *GetField(x, up);
-			}
-		}
-	}
-	m_ToBeCleaned.clear();
-	m_ToRemoveBlocks = false;
-
-	clearLinesMusic.stop();
-	m_Game.m_GameplayMusic.play();
 }
 
 void Board::MarkLinesForRemoval()
@@ -150,7 +21,7 @@ void Board::MarkLinesForRemoval()
 		for (int x = 0; x < m_Size.x; x++)
 		{
 			auto field = GetField(x, y);
-			if (field -> m_Occupied)
+			if (field->m_Occupied)
 				counter++;
 			if (counter == BOARD_WIDTH) // Line full
 			{
@@ -161,17 +32,7 @@ void Board::MarkLinesForRemoval()
 		}
 		counter = 0;
 	}
-	m_Game.m_Score.AddClearedLines(countClearedLines);
+	m_Game.m_score.AddClearedLines(countClearedLines);
 	std::sort(m_ToBeCleaned.begin(), m_ToBeCleaned.end(), [](int left, int right) { return left < right; });
-}
-
-void Board::Blink()
-{
-	//Speeds up blinking
-
-	int num = int(m_ElapsedTime * 5.f);
-	for (auto y : m_ToBeCleaned)
-		for (int x = 0; x < m_Size.x; x++)
-			GetField(x, y)->m_Visible = (num % 2 != 0);
 }
 
--- SMC_Project_Tetris++/SMC_Project_Tetris++/Board.h
+++ SMC_Project_Tetris++/SMC_Project_Tetris++/Board.h
@@ -1,61 +1,27 @@
 #pragma once
-#include "Game.h"
-#include "Config.h"
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 #include <array>
 #include <unordered_map>
+#include "IGame.h"
+#include "IBoard.h"
 
-class Game; // for using methods from class Game
+class IGame; // for using methods from class Game
 
-struct FieldInfo
-{
-	FieldInfo(sf::Texture&, uint16_t);
-	sf::Sprite m_Sprite;
-};
-
-struct Field
-{
-	Field& operator=(const Field&);
-	bool m_Occupied = false;
-	bool m_Visible = false;
-	FieldInfo* m_Info = nullptr;
-};
-
-
-class Board
+class Board : public IBoard
 {
 public:
 	using Position = sf::Vector2i;
 
 public:
-	Board(Position, Game&);
+	Board(Position, IGame&);
 	Board(const Board& other) = delete;
 	Board& operator=(const Board& other) = delete;
 
-	void Update(const sf::Time&);
-	void Clean();
-	void AddBlock(uint16_t, std::array<Position, 16>);
-	bool IsOccupied(std::array<Position, 16>);
-	void Draw(sf::RenderWindow&);
-	inline bool IsToRemoveBlocks() const { return m_ToRemoveBlocks; };
-	Field* GetField(uint16_t, uint16_t);
-
 private:
-	int Convert2DTo1D(uint16_t, uint16_t);
-	void CleanLines();
 	void MarkLinesForRemoval();
-	void Blink();
 
 private:
-	Game& m_Game;
-	std::unordered_map<uint16_t, std::unique_ptr<Field>> m_Fields;
-	std::unordered_map<uint16_t, std::unique_ptr<FieldInfo>> m_FieldInfos;
-	Position m_Size;
-	std::vector<int> m_ToBeCleaned;
-	float m_ElapsedTime;
-	bool m_ToRemoveBlocks;
 };
-
