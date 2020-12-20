083293b54a740e691db9d5fbf7997e73fbd0b81a
DESKTOP-KQRP2K4\user <george.codrut@yahoo.com>
2020-12-20 13:18:08 +0200

    Adding Board_2P class for multiplayer implementation

---
+++ SMC_Project_Tetris++/SMC_Project_Tetris++/Board_2P.h
@@ -0,0 +1,26 @@
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <array>
+#include <unordered_map>
+#include "IGame.h"
+#include "IBoard.h"
+
+class IGame; // for using methods from class Game
+
+class Board_2P : public IBoard
+{
+public:
+	using Position = sf::Vector2i;
+
+public:
+	Board_2P(Position, IGame&);
+	Board_2P(const Board_2P& other) = delete;
+	Board_2P& operator=(const Board_2P& other) = delete;
+
+private:
+	void MarkLinesForRemoval();
+
+private:
+};
